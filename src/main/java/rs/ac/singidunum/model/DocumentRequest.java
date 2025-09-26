cdc62f9f93a629a2d29853ac2b7a5effe587f65a
ZoranaVukadinovic <zorana.vukadinovic.21@singimail.rs>
2025-09-26 09:57:11 +0200

    commit

--- src/main/java/rs/ac/singidunum/model/DocumentRequest.java
+++ src/main/java/rs/ac/singidunum/model/DocumentRequest.java
@@ -1,9 +1,19 @@
 package rs.ac.singidunum.model;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import rs.ac.singidunum.model.enums.DocumentStatus;
 import rs.ac.singidunum.model.enums.DocumentType;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "document_requests")
